3de2ddb22b50c9123fb8e5aed229787f3a73046a
Rosa Figal <rosafigal@gmail.com>
2015-07-24 20:46:22 +0200

    Autenticacion

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var partials=require('express-partials');
 var routes = require('./routes/index');
 var methodOverride=require('method-override');
+var session=require('express-session');
 
 var app = express();
 
@@ -20,10 +21,23 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser('Quiz 2015'));
+app.use(session({
+                 secret:'Quiz 2015',
+                 resave:true,
+                 saveUninitialized:true
+                }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+           if(!req.path.match(/\/login|\/logout/)){
+              req.session.redir=req.path;
+            }
+          res.locals.session=req.session;
+          next();
+     });
 
 app.use('/', routes);
 
